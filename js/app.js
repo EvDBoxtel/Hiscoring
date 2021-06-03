d8452476afd6d91bfc1d23686cb618c4135dec28
Erik <erik.vandijk@student.fontys.nl>
2021-06-03 15:20:24 +0200

    Reset some files to origin

--- js/app.js
+++ js/app.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
 //Routes
 app.use('/', require('../routes/index'));
 app.use('/users', require('../routes/users'));
+app.use('/opdrachten', require('../routes/opdrachten.js'));
 
 const PORT = process.env.PORT || 5000;
 
